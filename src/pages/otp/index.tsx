1cdc9b661d7fcef65218d234fa22efa5684dface
Marshalsoft <ekene.marshall@aspacelifetech.com>
2024-02-14 05:53:42 +0100

    update

--- src/pages/otp/index.tsx
+++ src/pages/otp/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useRef, useState } from 'react'
 import { Logo } from '../../components/Logo'
 import { LogoDesign } from '../../components/LogoDesign'
